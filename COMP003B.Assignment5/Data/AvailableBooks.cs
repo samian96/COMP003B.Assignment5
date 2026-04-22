465e67e96e501b1b91e1be49849a0ad7d2932cb9
samian <sam.iannazzo@yahoo.com>
2026-04-21 19:32:44 -0700

    created data folder, added new library items

---
+++ COMP003B.Assignment5/Data/AvailableBooks.cs
@@ -0,0 +1,14 @@
+﻿using COMP003B.Assignment5.Models;
+
+namespace COMP003B.Assignment5.Data
+{
+    public static class AvailableBooks
+    {
+        public static List<Library> Library { get; } = new()
+        {
+            new Library { Id = 1, Name = "A Season In Hell", Price = 19.99m},
+            new Library { Id = 2, Name = "Dantes Inferno", Price = 48.99m},
+            new Library { Id = 3, Name = "Uzumaki", Price = 30.00m}
+        };
+    }
+}
