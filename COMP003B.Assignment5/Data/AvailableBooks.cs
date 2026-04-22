a681692c661fdd474174d1692d46fb8f9de98ba9
samian <sam.iannazzo@yahoo.com>
2026-04-21 19:39:34 -0700

    added utcNow to DateTime

--- COMP003B.Assignment5/Data/AvailableBooks.cs
+++ COMP003B.Assignment5/Data/AvailableBooks.cs
@@ -6,7 +6,7 @@ namespace COMP003B.Assignment5.Data
     {
         public static List<Library> Library { get; } = new()
         {
-            new Library { Id = 1, Name = "A Season In Hell", Price = 19.99m},
+            new Library { Id = 1, Name = "A Season In Hell", Price = 19.99m },
             new Library { Id = 2, Name = "Dantes Inferno", Price = 48.99m},
             new Library { Id = 3, Name = "Uzumaki", Price = 30.00m}
         };
--- COMP003B.Assignment5/Models/Library.cs
+++ COMP003B.Assignment5/Models/Library.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public DateTime ScheduleDate { get; set; }
+        public DateTime ScheduleDate { get; set; } = DateTime.UtcNow;
     }
 }
