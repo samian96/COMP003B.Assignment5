e1fb5199183b8c887fb4f5300424cee395ae2f62
samian <sam.iannazzo@yahoo.com>
2026-04-21 19:25:53 -0700

    created model Library added int string decimal datetime

---
+++ COMP003B.Assignment5/Models/Library.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003B.Assignment5.Models
+{
+    public class Library
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public DateTime ScheduleDate { get; set; }
+    }
+}
