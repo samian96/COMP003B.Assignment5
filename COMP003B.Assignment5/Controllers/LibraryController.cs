80098d3aef6bd4487b273bdedeb8fce227e3268e
samian <sam.iannazzo@yahoo.com>
2026-04-21 21:25:57 -0700

    Created library controller

---
+++ COMP003B.Assignment5/Controllers/LibraryController.cs
@@ -0,0 +1,14 @@
+﻿using COMP003B.Assignment5.Data;
+using COMP003B.Assignment5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment5.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
